3b0b3ae8a4baaf352abab44d6e81a0132062edff
Yash  Pulse <pulse.yash2905@gmail.com>
2024-10-27 22:08:11 +1100

    Final Code Commit

--- api/library/compressor.py
+++ api/library/compressor.py
@@ -5,6 +5,8 @@ from tqdm import tqdm
 from PIL import Image
 import time
 import json
+import numpy as np
+from tensorflow import keras
 
 class Compressor:
     
@@ -53,6 +55,44 @@ class Compressor:
                 )                
             img.save(f"{output_folder}/{file_name.replace('.tif','')}.jpg", "JPEG", quality=quality)
             # Update the compressed image path and size in the database
+            end_time = time.perf_counter()
+            with self.__connection:   
+                self.__connection.execute(
+                    """UPDATE image_data 
+                    SET compressed_image_path = ?, 
+                    compressed_image_size = ?,
+                    compression_time = ? 
+                    WHERE input_image_path = ? AND run_id = ?""",
+                    (f"{output_folder}/{file_name.replace('.tif', '')}.jpg", 
+                     os.path.getsize(f"{output_folder}/{file_name.replace('.tif', '')}.jpg"), 
+                     (end_time - start_time) * 1_000_000,
+                     input_path, 
+                     run_id)
+                )
+
+    def __compress_dl_encoder(self,input_path:str, output_folder:str,run_id:str):
+        file_name = input_path.split("/")[-1]
+        input_file_size = os.path.getsize(input_path)
+        start_time = time.perf_counter()
+        with Image.open(input_path) as img:   
+            print(img.height,img.width)  
+            with self.__connection:      
+                self.__connection.execute(
+                    """INSERT INTO image_data (run_id, height,width,input_image_path, input_image_size) VALUES (?,?,?, ?, ?)""",
+                    (run_id,img.height,img.width, input_path,input_file_size )
+                )                            
+            
+
+            model = keras.models.load_model('encoder_input.h5')
+            img = img.resize((256,256))
+            img_array = np.array(img)
+            img_array = img_array/255.0
+            img_array = img_array.reshape(1,256,256,3)
+            img_array = model.predict(img_array)
+            img_array = img_array*255
+            img_array = img_array.reshape(256,256,3)
+            img_array = Image.fromarray(img_array.astype('uint8'))
+            img_array.save(f"{output_folder}/{file_name.replace('.tif','')}.jpg", "JPEG")
             end_time = time.perf_counter()
             with self.__connection:   
                 self.__connection.execute(
@@ -128,11 +168,11 @@ class Compressor:
                 yield str(e)
                 continue
 
-    def compress_dl_encoder(self,input_path:str, output_folder:str,quality:int,run_id:str):
+    def compress_dl_encoder(self,input_path:str, output_folder:str,run_id:str):
         
         # if the input path is a file, convert that file
         if os.path.isfile(input_path):
-            self.__compress_jpeg(input_path,output_folder,quality,run_id)
+            self.__compress_dl_encoder(input_path,output_folder,run_id)
             return
         # Get all tiff files in the folder even in subdirectories
         tiff_files = glob.glob(f"{input_path}/**/*.tif", recursive=True)
@@ -149,7 +189,7 @@ class Compressor:
 
         for tiff_file in tqdm(tiff_files):
             try:
-                self.__compress_jpeg(tiff_file,output_folder,quality,run_id)
+                self.__compress_dl_encoder(tiff_file,output_folder,run_id)
                 to_return["success"] += 1
                 yield json.dumps(to_return)
             except Exception as e:
--- api/library/decompressor.py
+++ api/library/decompressor.py
@@ -1,10 +1,12 @@
 import glob
 import os
 import sqlite3
+import numpy as np
 from tqdm import tqdm
 from PIL import Image
 import time
 import json
+from tensorflow import keras
 
 
 class Decompressor:
@@ -39,11 +41,21 @@ class Decompressor:
                 input_path)
             )
                 
-    def __decompress_jpeg(self,input_path:str, output_folder:str,run_id:str):
+    
+    def __decompress_dl_decoder(self,input_path:str, output_folder:str,run_id:str):
         file_name = input_path.split("/")[-1]
         start_time = time.perf_counter()
         with Image.open(input_path) as img:
-            img.save(f"{output_folder}/{file_name.replace('.jpg','')}.tif", format="TIFF")
+            model = keras.models.load_model('decoder_output.h5')
+            img = img.resize((256,256))
+            img_array = np.array(img)
+            img_array = img_array/255.0
+            img_array = img_array.reshape(1,256,256,3)
+            img_array = model.predict(img_array)
+            img_array = img_array*255
+            img_array = img_array.reshape(256,256,3)
+            img_array = Image.fromarray(img_array.astype('uint8'))
+            img_array.save(f"{output_folder}/{file_name.replace('.tif','')}.jpg", "JPEG")
             # self.__connection.execute(
             #     "CREATE TABLE IF NOT EXISTS image_data (id INTEGER PRIMARY KEY AUTOINCREMENT,run_id TEXT,input_image_path TEXT, compressed_image_path TEXT, noisy_image_path TEXT,decompressed_image_path TEXT, input_image_size INTEGER, compressed_image_size INTEGER, noisy_image_size INTEGER, decompressed_image_size INTEGER)"
             # )
@@ -130,7 +142,7 @@ class Decompressor:
         
         # if the input path is a file, convert that file
         if os.path.isfile(input_path):
-            self.__decompress_jpeg(input_path,output_folder,run_id)
+            self.__decompress_dl_decoder(input_path,output_folder,run_id)
             return
 
         # Get all tiff files in the folder even in subdirectories
@@ -148,7 +160,7 @@ class Decompressor:
 
         for tiff_file in tqdm(tiff_files):
             try:
-                self.__decompress_jpeg(tiff_file,output_folder,run_id)
+                self.__decompress_dl_decoder(tiff_file,output_folder,run_id)
                 to_return["success"] += 1
                 yield json.dumps(to_return)
             except Exception as e:
